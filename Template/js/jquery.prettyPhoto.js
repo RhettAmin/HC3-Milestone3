bc275f166f99964658b7b46bbd6eed82a2f19a38
David Elsonbaty <davidels@me.com>
2014-12-11 15:21:10 -0500

    Profile Page

--- Template/js/jquery.prettyPhoto.js
+++ Template/js/jquery.prettyPhoto.js
@@ -10,7 +10,7 @@
 	
 	$.fn.prettyPhoto = function(pp_settings) {
 		pp_settings = jQuery.extend({
-			animation_speed: 'fast', /* fast/slow/normal */
+			animation_speed: 'normal', /* fast/slow/normal */
 			slideshow: 5000, /* false OR interval time in ms */
 			autoplay_slideshow: false, /* true/false */
 			opacity: 0.80, /* Value between 0 and 1 */
@@ -21,7 +21,7 @@
 			counter_separator_label: '/', /* The separator for the gallery counter 1 "of" 2 */
 			theme: 'pp_default', /* light_rounded / dark_rounded / light_square / dark_square / facebook */
 			horizontal_padding: 20, /* The padding on each side of the picture */
-			hideflash: false, /* Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto */
+			hideflash: true, /* Hides all the flash object on a page, set to TRUE if flash appears over prettyPhoto */
 			wmode: 'opaque', /* Set the flash wmode attribute */
 			autoplay: true, /* Automatically start videos: True/False */
 			modal: false, /* If set to true, only the close button will close the window */
